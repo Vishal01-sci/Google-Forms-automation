ba59d0bec2e29cd869edd42512699d7a95fb0472
Vishal01-sci <nsvishal07@gmail.com>
2025-12-10 15:22:25 +0530

    ME_QA_XGOOGLE_FORM_MODULE_L1

--- src/test/java/demo/TestCases.java
+++ src/test/java/demo/TestCases.java
@@ -9,19 +9,84 @@ import org.openqa.selenium.logging.LoggingPreferences;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
+import org.testng.Assert;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import java.time.Duration;
 
+import java.beans.Transient;
+import java.time.Duration;
 import java.util.logging.Level;
-// import io.github.bonigarcia.wdm.WebDriverManager;
+//import io.github.bonigarcia.wdm.WebDriverManager;
 import demo.wrappers.Wrappers;
 
 public class TestCases {
-    ChromeDriver driver;
+    public static ChromeDriver driver;
+    
+
 
     /*
      * TODO: Write your tests here with testng @Test annotation. 
      * Follow `testCase01` `testCase02`... format or what is provided in instructions
      */
 
+    @Test
+    public void testCase01() throws InterruptedException{
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
+        // Navigating to form
+        Wrappers navigateToForm = new Wrappers(driver);
+        navigateToForm.navigateToForms();
+        String currentURL = driver.getCurrentUrl();
+        System.out.println(currentURL);
+        Boolean status = currentURL.contains("forms");
+        Assert.assertTrue(status, "Failed to navigate to forms page");
+
+        // Entering name 
+        Wrappers firstText = new Wrappers(driver);
+        firstText.enterName("Crio Learner");
+
+        // Entering reason
+        Wrappers secondText = new Wrappers(driver);
+        Long epoch = System.currentTimeMillis()/1000;
+        String textToBeEntered = "I want to be the best QA Engineer! " + Long.toString(epoch);
+        secondText.enterReason(textToBeEntered);
+        
+
+        // Selecting experience
+        Wrappers selectExperience = new Wrappers(driver);
+        selectExperience.experienceInAutomation();
+        System.out.println("Experience 3-5 years is selected");
+        
+
+        // Selectig skills learned
+        Wrappers selectSkills = new Wrappers(driver);
+        selectSkills.selectSkills();
+        System.out.println("Skills - Java, Selenium and TestNG is selected");
+        
+
+        // Selecting how to address you
+        Wrappers sltMr = new Wrappers(driver);
+        sltMr.selectMR();
+        System.out.println("Mr is selected");
+        
+
+        // Selectig date
+        Wrappers selectDate = new Wrappers(driver);
+        selectDate.sltDate();
+
+        // Entering time
+        Wrappers enterTime = new Wrappers(driver);
+        enterTime.entTime();
+
+        // Submiting form
+        Wrappers submitForm = new Wrappers(driver);
+        submitForm.clickSubmit();
+
+        // Validating success message
+        Wrappers valMessage = new Wrappers(driver);
+        valMessage.thanksMessage();
+    }
      
     /*
      * Do not change the provided methods unless necessary, they will help in automation and assessment
--- src/test/java/demo/wrappers/Wrappers.java
+++ src/test/java/demo/wrappers/Wrappers.java
@@ -1,6 +1,7 @@
 package demo.wrappers;
 
 import java.time.Instant;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
@@ -11,9 +12,122 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import java.time.Duration;
+import org.openqa.selenium.Keys;
+import java.util.List;
+import java.util.ArrayList;
 
 public class Wrappers {
     /*
      * Write your selenium wrappers here
      */
+    ChromeDriver driver;
+    public Wrappers(ChromeDriver driver){
+        this.driver = driver;
+    }
+
+    WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(3));
+
+    String url = "https://forms.gle/wjPkzeSEk1CM7KgGA";
+
+    // Navigating to form
+    public void navigateToForms(){
+        driver.get(this.url);
+    }
+
+    // Entering name 
+    public void enterName(String name){
+        WebElement nameText = driver.findElement(By.xpath("//div[@class='Xb9hP']//input[1]"));
+        nameText.sendKeys(name);
+    }
+
+    // Entering reason
+    public void enterReason(String text){
+        WebElement reasonText = driver.findElement(By.xpath("//textarea[@class='KHxj8b tL9Q4c']"));
+        reasonText.sendKeys(text);
+    }
+
+    // Selecting experience
+    public void experienceInAutomation(){
+        WebElement radioButton = driver.findElement(By.xpath("(//div[@class='AB7Lab Id5V1'])[2]"));
+        wait.until(ExpectedConditions.elementToBeClickable(radioButton));
+        radioButton.click();
+        
+    }
+
+    // Selectig skills learned
+    public void selectSkills(){
+        WebElement selectJava = driver.findElement(By.xpath("(//div[@class='bzfPab wFGF8']//div[@role='checkbox'])[1]"));
+        wait.until(ExpectedConditions.elementToBeClickable(selectJava));
+        selectJava.click();
+        WebElement selectSelenium = driver.findElement(By.xpath("(//div[@class='bzfPab wFGF8']//div[@role='checkbox'])[2]"));
+        wait.until(ExpectedConditions.elementToBeClickable(selectSelenium));
+        selectSelenium.click();
+        WebElement selectTestNG = driver.findElement(By.xpath("(//div[@class='bzfPab wFGF8']//div[@role='checkbox'])[4]"));
+        wait.until(ExpectedConditions.elementToBeClickable(selectTestNG));
+        selectTestNG.click();
+        
+    }
+
+    // Selecting how to address you
+    public void selectMR() throws InterruptedException{
+        WebElement dropDown = driver.findElement(By.xpath("(//div[contains(@class,'MocG8c ')])[1]"));
+        //dropDown.click();
+        dropDown.sendKeys("Mr", Keys.ENTER);
+        Thread.sleep(3000);
+        // List<WebElement> list = driver.findElements(By.xpath("//div[contains(@class,'OA0qNb ')]/div"));
+        // for(WebElement li : list){
+        //     if(li.getText().equals("Mr")){
+        //         li.click();
+        //         break;
+        //     }
+        // }
+        // Thread.sleep(3000);git 
+        
+
+        // WebElement selMR = driver.findElement(By.xpath("//div[contains(@class,'MocG8c ')][2]//span"));
+        // selMR.click();
+    }
+
+    // Selectig date
+    public void sltDate() throws InterruptedException{
+        LocalDate date = LocalDate.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+        String currentDate = date.format(formatter);
+        System.out.println(currentDate);
+        String actualDate = date.minusDays(7).format(formatter);
+        WebElement enterDate = driver.findElement(By.xpath("//input[@type = 'date']"));
+        enterDate.sendKeys(actualDate);
+        System.out.println(actualDate);
+    }
+
+    // Entering time
+    public void entTime(){
+        WebElement enterTime1 = driver.findElement(By.xpath("(//input[@type ='text' and contains(@class,'whsOnd ')])[2]"));
+        enterTime1.sendKeys("07");
+        WebElement enterTime2 = driver.findElement(By.xpath("(//input[@type ='text' and contains(@class,'whsOnd ')])[3]"));
+        enterTime2.sendKeys("30");
+    }
+
+    // Submiting form
+    public void clickSubmit(){
+        WebElement clkSubmit = driver.findElement(By.xpath("(//span[contains(text(),'Submit')])"));
+        clkSubmit.click();
+    }
+
+    // Validating success message
+    public void thanksMessage(){
+        String expectedMessage = "Thanks for your response, Automation Wizard!";
+        WebElement message = driver.findElement(By.xpath("//div[@class='vHW8K']"));
+        String actualMessage = message.getText();
+        if(actualMessage.contains(expectedMessage)){
+            System.out.println("Expected message is present");
+        }else{
+            System.out.println("Expected message is not present");
+        }
+
+    }
+
+
+
+    
 }
